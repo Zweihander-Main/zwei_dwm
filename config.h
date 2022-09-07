a395d913df90b95f0f8679cff8aad830cf4702a8
Zweihänder <zweidev@zweihander.me>
2022-09-06 20:58:36 -0400

    fix(config): update tb class

--- config.h
+++ config.h
@@ -34,10 +34,9 @@ static const Rule rules[] = {
 	 */
 	/* class         instance    title       tags mask     isfloating   monitor  float xywh     borderpx */
 	{ "copyq",       NULL,       NULL,       0,            1,           -1 ,     50,50,500,500, 1},
-	{ "Thunderbird", NULL,       NULL,       0,            0,           2 ,      -1,-1,-1,-1, 1},
 	{ "Element",     NULL,       NULL,       1 << 8,       0,           3 ,      -1,-1,-1,-1, 1},
 	{ "Mumble",      NULL,       NULL,       1 << 8,       0,           0 ,      -1,-1,-1,-1, 1},
-	{ "Thunderbird", NULL,       NULL,       1 << 7,       0,           2 ,      -1,-1,-1,-1, 1},
+	{ "thunderbird", NULL,       NULL,       1 << 7,       0,           2 ,      -1,-1,-1,-1, 1},
 	{ "stalonetray", NULL,       NULL,       0,            1,           2 ,      50,50,500,500, 0},
 	{ "pinentry-qt", NULL,       NULL,       0,            1,           -1 ,     -1,-1,-1,-1, 1}
 
